2890d6053170478e7a572b1be93761b41aae63fb
Yusska <83473942+YusifMemmedzade@users.noreply.github.com>
2021-05-02 20:30:24 +0400

    Create 0017 - Gardener Painter - Rəssam Bağban - Садовник-художник.cpp

---
+++ 0000-0999/0017 - Gardener Painter - Rəssam Bağban - Садовник-художник.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int x,y;
+	// Input X
+	cin>>x;
+	y=3;
+	// A little bit of Logic
+	for(int i=1;i<x;i++) y*=2;
+	
+	cout<<y;
+}
