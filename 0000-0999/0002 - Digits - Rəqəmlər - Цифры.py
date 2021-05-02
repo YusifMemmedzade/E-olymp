941935e054616b74634abec5f3842e6627d2f248
Yusska <83473942+YusifMemmedzade@users.noreply.github.com>
2021-05-02 16:47:31 +0400

    Create 0002 - Digits - Rəqəmlər - Цифры.py

---
+++ 0000-0999/0002 - Digits - Rəqəmlər - Цифры.py
@@ -0,0 +1,6 @@
+# First Input X as intenger
+x = int(input())
+
+# Then convert x to string for use len method (This is works on only positive numbers)
+# If you wanna use this with negative number just subtract 1 Example:| Input: -123; result = 4
+print( len(str(x)) )
