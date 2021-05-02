78631aa45e2e6d3a20805188fc9359bb4179e7f3
Yusska <83473942+YusifMemmedzade@users.noreply.github.com>
2021-05-02 16:32:50 +0400

    Create 0001 - Simple Problem - Sadə Məsələ - Простая задача.cpp

---
+++ 0000-0999/0001 - Simple Problem - Sadə Məsələ - Простая задача.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	int x;
+  
+	// Input X 
+	cin>>x;
+  
+	// X is int , Example 23 / 10 = 2 ; Then 23 % 10 = 3 ; thats all we need and one space between them
+	cout<< x/10 <<" "<< x%10 ;
+  
+}
