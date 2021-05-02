6842577390e3a1b5aa41b6b07a659a5696e8df00
Yusska <83473942+YusifMemmedzade@users.noreply.github.com>
2021-05-02 16:54:41 +0400

    Create 0002 - Digits - Rəqəmlər - Цифры.cpp

---
+++ 0000-0999/0002 - Digits - Rəqəmlər - Цифры.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int sum=0;
+	string x;
+	//Input X as String
+	cin>>x;
+	//I Created For loop and this loop continues as long as length of x, so we can count
+	for(int i=0; i<x.length();i++){
+		sum++;
+	}
+	  cout<<sum;
+}
